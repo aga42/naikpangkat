e3df958cb75bda7541be99d9fbb2f8b751a8048d
aga42 <itu.aga42@gmail.com>
2016-10-07 01:54:14 +0700

    misi dummy UI

--- app/src/main/java/com/barethitam/naikpangkat/view/fragment/MisiFragment.java
+++ app/src/main/java/com/barethitam/naikpangkat/view/fragment/MisiFragment.java
@@ -3,26 +3,33 @@ package com.barethitam.naikpangkat.view.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.barethitam.naikpangkat.R;
+import com.barethitam.naikpangkat.view.adapter.MisiAdapter;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
  * Created by LTE on 10/6/2016.
  */
-public class MisiFragment extends Fragment {
+public class MisiFragment extends Fragment implements MisiAdapter.ItemSelectedListener{
 
     private static MisiFragment instance;
+    MisiAdapter adapter;
+    @BindView(R.id.recycleView)
+    RecyclerView recycleView;
 
     public static MisiFragment getInstance(Bundle bundle) {
         if (null == instance) {
             instance = new MisiFragment();
             instance.setArguments(bundle);
-        }else{
+        } else {
             instance.getArguments().putAll(bundle);
         }
         return instance;
@@ -39,7 +46,14 @@ public class MisiFragment extends Fragment {
         return v;
     }
 
-    private void initView(){
+    private void initView() {
+        adapter = new MisiAdapter(getContext(), this);
+        recycleView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recycleView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemSelected() {
 
     }
 }
