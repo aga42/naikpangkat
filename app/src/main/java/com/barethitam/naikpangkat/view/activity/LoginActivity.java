e9495ad682b8cb08313c458855b4e684beb819d0
aga42 <itu.aga42@gmail.com>
2016-10-07 00:26:47 +0700

    login UI

--- app/src/main/java/com/barethitam/naikpangkat/view/activity/LoginActivity.java
+++ app/src/main/java/com/barethitam/naikpangkat/view/activity/LoginActivity.java
@@ -9,11 +9,11 @@ import com.barethitam.naikpangkat.R;
 /**
  * Created by LTE on 10/6/2016.
  */
-public class MisiDetailActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_misi_detail);
+        setContentView(R.layout.activity_login);
     }
 }
